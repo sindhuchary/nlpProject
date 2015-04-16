aa11575b8f9c2897c7644dd825fa656cc91bf3ec
ess15 <m153762@usna.edu>
2015-04-16 17:04:17 -0400

    finished the make vectors function and slightly modified the programs under the directory util so we can use them

---
+++ src/Vectorizer.java
@@ -0,0 +1,44 @@
+//ReviewSummarizer
+//Tagger.java
+//Tian, Khochtali, Hudson
+//NLP/Chambers
+
+//Imports
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import util.Counter;
+
+public class Vectorizer {
+
+  public Vectorizer( ) {//constructor
+  }
+
+  //input is hashmap of ID's and list of sentences
+  public HashMap<String, ArrayList<Counter<String>> > makeVectors(HashMap<String, ArrayList< List<String> > > hm) {    
+    HashMap<String, ArrayList<Counter<String>> > retHM = new HashMap<String, ArrayList<Counter<String>> >();
+    for(String ID : hm.keySet() ){
+        ArrayList<Counter<String>> tempVectList=new ArrayList<Counter<String>>();
+        //hm.get(ID) returns back List< List<String> > 
+        for(List<String> sentence : hm.get(ID) ){//loop through the list of sentences for that specific ID 
+          Counter tempVect = new Counter<String>();
+          for(String word : sentence){//loop through the sentence
+            tempVect.incrementCount(word,1.0);
+          }
+          tempVectList.add(tempVect);
+        }
+        retHM.put(ID,tempVectList);
+    }
+    return retHM;
+  }
+
+  //RemoveNoise takes the tagged sentences and returns a list of vectors containing words essential to the review sentence
+  
+  
+  public static void main( String[] args ) throws Exception {
+     
+     
+  }
+}
+
+
---
+++ src/util/IdentityCounter.java~
@@ -0,0 +1,14 @@
+package usna.util;
+
+/**
+ * Convenience Extension of Counter to use an IdentityHashMap.
+ *
+ * @author Dan Klein
+ */
+public class IdentityCounter<E> extends Counter<E> {
+  static final long serialVersionUID = 1L;
+
+  public IdentityCounter() {
+    super(new MapFactory.IdentityHashMapFactory<E,Double>());
+  }
+}
